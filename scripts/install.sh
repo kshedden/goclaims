bf905e5e97c820302a369ae84d6e96613d54b6d3
Kerby Shedden <kshedden@umich.edu>
2017-04-12 12:59:56 -0400

    update

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,3 +3,5 @@
 go install github.com/kshedden/goclaims/sastocols
 go install github.com/kshedden/goclaims/factorize
 go install github.com/kshedden/goclaims/sortbuckets
+go install github.com/kshedden/goclaims/config
+go install github.com/kshedden/goclaims/tools/qperson
